0279f05422c66ee59434520cc1ced4ee012bef8f
Kyle Mitchell <kyle@kemitchell.com>
2015-08-28 09:21:46 -0700

    Condense code style

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-var alpha = require('lower-alpha');
+var alpha = require('lower-alpha')
 
 module.exports = function(numbering, shortForm) {
-  return numbering
-    .map(function(component) {
-      var element = component.element;
-      var series = component.series;
-      return series.of > 1 ?
-        alpha(series.number) + '-' + element.number :
-        element.number;
-    })
-    .join('.')
-    .toUpperCase() +
-    ( shortForm ? '.' : '' );
-};
+  return (
+    numbering
+      .map(function(component) {
+        var element = component.element
+        var series = component.series
+        return (
+          series.of > 1 ?
+            ( alpha(series.number) + '-' + element.number ) :
+            element.number ) })
+      .join('.')
+      .toUpperCase() +
+    ( shortForm ? '.' : '' ) ) }
